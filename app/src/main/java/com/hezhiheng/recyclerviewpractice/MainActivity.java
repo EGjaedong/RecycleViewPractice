5d3d834eeb066c392919e28e2ab0b02603848bbc
unknown <hzh_innovation@163.com>
2020-08-17 20:45:49 +0800

    [hzh] feat: add btn_to_multi_type_activity click event

--- app/src/main/java/com/hezhiheng/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/hezhiheng/recyclerviewpractice/MainActivity.java
@@ -13,6 +13,8 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btn_to_simple_type_activity)
     Button btnToSimpleTypeDataListActivity;
+    @BindView(R.id.btn_to_multi_type_activity)
+    Button btnToMultiTypeDataListActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.btn_to_simple_type_activity)
-    public void btnRecyclerViewClick(Button button){
-        Intent intent = new Intent(MainActivity.this, SimpleTypeDataListActivity.class);
-        startActivity(intent);
+    public void btnRecyclerViewClick(Button button) {
+        switch (button.getId()) {
+            case R.id.btn_to_simple_type_activity:
+                startActivity(new Intent(MainActivity.this, SimpleTypeDataListActivity.class));
+                break;
+            case R.id.btn_to_multi_type_activity:
+                startActivity(new Intent(MainActivity.this, MultiTypeDataListActivity.class));
+                break;
+        }
     }
 }
