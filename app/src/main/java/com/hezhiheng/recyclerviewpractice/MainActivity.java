3e1e35ac64956dd03bd1cada4a1bc6670344d461
unknown <hzh_innovation@163.com>
2020-08-17 23:34:30 +0800

    [hzh] feat: delete useless code from MainActivity

--- app/src/main/java/com/hezhiheng/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/hezhiheng/recyclerviewpractice/MainActivity.java
@@ -3,7 +3,6 @@ package com.hezhiheng.recyclerviewpractice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,8 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        ImageView image = findViewById(R.id.data_avatar_main);
     }
 
     @OnClick({R.id.btn_to_simple_type_activity,
