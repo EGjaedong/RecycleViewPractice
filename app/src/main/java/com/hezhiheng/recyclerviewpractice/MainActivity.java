fcd62432a32d6dfc974d8eea41e244b9a70edcb4
unknown <hzh_innovation@163.com>
2020-08-17 13:03:36 +0800

    [hzh] feat: click button open RecyclerView layout

--- app/src/main/java/com/hezhiheng/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/hezhiheng/recyclerviewpractice/MainActivity.java
@@ -2,13 +2,28 @@ package com.hezhiheng.recyclerviewpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btn_recycler_view)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_recycler_view)
+    public void btnRecyclerViewClick(Button button){
+        Intent intent = new Intent(MainActivity.this, RecyclerViewActivity.class);
+        startActivity(intent);
     }
 }
