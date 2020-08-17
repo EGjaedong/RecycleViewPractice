da1d32663858af41ea249c754f01344ae39a76de
unknown <hzh_innovation@163.com>
2020-08-17 20:17:51 +0800

    [hzh] feat: add MultiTypeDataListActivity

---
+++ app/src/main/java/com/hezhiheng/recyclerviewpractice/MultiTypeDataListActivity.java
@@ -0,0 +1,14 @@
+package com.hezhiheng.recyclerviewpractice;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MultiTypeDataListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycler_view_activity);
+    }
+}
