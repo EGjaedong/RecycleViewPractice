d0104f3210c75c95868ab52f73a1c7a58efe2631
unknown <hzh_innovation@163.com>
2020-08-18 16:55:03 +0800

    [hzh] feat: refactor MuliTypeDataListActivity and MultiTypeWithImageDataListActivity extends AbstractDataListActivity

--- app/src/main/java/com/hezhiheng/recyclerviewpractice/MultiTypeWithImageDataListActivity.java
+++ app/src/main/java/com/hezhiheng/recyclerviewpractice/MultiTypeWithImageDataListActivity.java
@@ -1,47 +1,21 @@
 package com.hezhiheng.recyclerviewpractice;
 
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.hezhiheng.recyclerviewpractice.adapter.MultiTypeDataAdapter;
 import com.hezhiheng.recyclerviewpractice.domain.Data;
 
-import java.util.List;
-
-import butterknife.BindDimen;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MultiTypeWithImageDataListActivity extends AppCompatActivity {
+public class MultiTypeWithImageDataListActivity extends AbstractDataListActivity {
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
-    @BindDimen(R.dimen.recycler_view_item_space)
-    int itemSpace;
 
-    List<Data> dataList;
-
-    static class SpacesItemDecoration extends RecyclerView.ItemDecoration {
-        private int space;
-
-        public SpacesItemDecoration(int space) {
-            this.space = space;
-        }
-
-        @Override
-        public void getItemOffsets(@NonNull Rect outRect, @NonNull View view,
-                                   @NonNull RecyclerView parent,
-                                   @NonNull RecyclerView.State state) {
-            outRect.top = space;
-            outRect.bottom = space;
-        }
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
